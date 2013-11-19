104555b26979f7f5038e1f91724cfc8a4f64de79
wotozone <xoduszzong@gmail.com>
2013-11-18 23:18:49 -0800

    1.0.11.18
    start coding

---
+++ ProjectV2_Monopoly/ProjectV2_Monopoly/main.cpp
@@ -0,0 +1,34 @@
+
+/*
+* File:   main.cpp
+* Author: TaeYoun Kim
+* Created by Minji Kim on 2013. 11. 18..
+* programming tools
+* 1.0.11.18 //Xcode on OSX
+* source -
+*/
+
+/*
+ * PATCH NOTE
+ * 1.0.11.18 - Setup libraries and basic settings
+ */
+
+//Libraries
+#include <cstdlib>
+#include <iostream>
+#include <iomanip>
+#include <string>
+#include <sstream>
+#include <cmath>
+#include <ctime>
+using namespace std;
+
+//Global Constants
+const string VERSION="1.0.11.18";
+
+//function Prototypes
+
+int main(int argc, const char * argv[]){
+    
+}
+
